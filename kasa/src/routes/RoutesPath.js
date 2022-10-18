f0d3601c83cafa7629c455c33e4b04200f52774f
alex24w <alex24w@gmail.com>
2022-10-18 11:10:08 +0200

    a propos page

--- kasa/src/routes/RoutesPath.js
+++ kasa/src/routes/RoutesPath.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout"
 import Home from "../pages/Home";
 import Logement from '../pages/Logement';
+import APropos from "../pages/APropos";
 
 
 
@@ -13,6 +14,7 @@ function RoutesPath() {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/logement/:id" element={<Logement/>}/>
+                    <Route path="/a-propos" element={<APropos />}/>
                 </Routes>
             </Layout>
         </BrowserRouter>
