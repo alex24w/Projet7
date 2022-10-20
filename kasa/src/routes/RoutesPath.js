5ce0f891fd3d78b82b45be9ee5db89a551cfed78
alex24w <alex24w@gmail.com>
2022-10-20 16:24:19 +0200

    404

--- kasa/src/routes/RoutesPath.js
+++ kasa/src/routes/RoutesPath.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout"
 import Home from "../pages/Home";
 import Logement from '../pages/Logement';
 import APropos from "../pages/APropos";
+import Erreur404 from "../pages/404";
 
 
 
@@ -15,6 +16,7 @@ function RoutesPath() {
                     <Route path="/" element={<Home/>}/>
                     <Route path="/logement/:id" element={<Logement/>}/>
                     <Route path="/a-propos" element={<APropos/>}/>
+                    <Route path="*" element={<Erreur404 />}/>
                 </Routes>
             </Layout>
         </BrowserRouter>
